2ff7d99c34fd136dcfdaf7aaa603537cb072ee78
psavan1655 <savan.gatesaniya@gmail.com>
2023-04-29 01:19:47 +0530

    hero-section complete

--- src/pages/demo.tsx
+++ src/pages/demo.tsx
@@ -1,7 +1,12 @@
+import Link from "next/link";
 import React from "react";
 
 const Demo = () => {
-  return <div>Demo</div>;
+  return (
+    <div>
+      <Link href={"/"}>Home</Link>
+    </div>
+  );
 };
 
 export default Demo;
