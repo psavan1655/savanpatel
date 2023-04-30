858cfa905468e482529183bb32604cb53fab4e7c
psavan1655 <savan.gatesaniya@gmail.com>
2023-05-01 01:20:09 +0530

    footer fix

--- src/modules/common/components/footer.tsx
+++ src/modules/common/components/footer.tsx
@@ -4,11 +4,10 @@ import { motion, useScroll, useTransform } from "framer-motion";
 const Footer = () => {
   const { scrollYProgress } = useScroll();
 
-  const x = useTransform(scrollYProgress, [0, 1], ["100%", "0%"]);
+  const x = useTransform(scrollYProgress, [0.8, 1], ["10%", "0%"]);
 
   return (
     <footer className="flex flex-col min-h-[90vh] bg-[#1c1d20]">
-      <motion.div className="absolute h-[50px] w-full bg-white"></motion.div>
       <div className="flex flex-col place-self-center my-44 w-[70%]">
         <div className="text-[90px]">
           Let&apos;s work
@@ -21,23 +20,20 @@ const Footer = () => {
             whileInView="visible"
             viewport={{ once: true }}
             transition={{
-              ease: "easeOut",
+              ease: "easeInOut",
               duration: 0.2,
             }}
-            // variants={{
-            //   hidden: { right: "20%" },
-            //   visible: { right: "0%" },
-            // }}
             style={{
               right: x,
             }}
-            className="get-in-touch h-[170px] w-[170px] bg-[#455ced] rounded-full absolute right-0"
+            className="get-in-touch h-[170px] w-[170px] bg-[#455ced] footer-circle-liquid rounded-full absolute right-0"
           >
-            <div className="flex justify-center items-center h-full w-full">
+            <span className="absolute flex justify-center items-center h-full w-full">
               Get in touch
-            </div>
+            </span>
           </motion.div>
         </div>
+
         <div>Mail, Number</div>
       </div>
       <div>Labels</div>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,27 @@
-import { useEffect, useLayoutEffect, useRef } from "react";
-
-import { AnimatePresence } from "framer-motion";
+import { AnimatePresence, useScroll, useTransform } from "framer-motion";
 import type { AppProps } from "next/app";
 
 import "@/styles/globals.css";
 import "@/styles/styles.css";
 import Footer from "@/modules/common/components/footer";
+import { motion } from "framer-motion";
 
 export default function App({ Component, pageProps, router }: AppProps) {
+  const { scrollYProgress } = useScroll();
+  const x = useTransform(scrollYProgress, [0.8, 1], ["300px", "30px"]);
+  {
+    console.log(x.get());
+  }
+
   return (
     <main className="main" id="smooth-wrapper">
       <AnimatePresence>
         <div id="smooth-content" className="main-content">
           <Component key={router.pathname} {...pageProps} />
+          <motion.div
+            className="h-[100px] w-full text-red text-4xl rounded-full bg-white rounded-div-illusion"
+            style={{ height: x }}
+          ></motion.div>
         </div>
         <Footer />
       </AnimatePresence>
